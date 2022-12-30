250cbdd56d4943b09f259c1de7f50e8af6b3a9dc
GiulianaDeriu <102539934+GiulianaDeriu@users.noreply.github.com>
2022-12-30 12:37:27 +0100

    4 files modified, 6 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,15 +3,26 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
 
+import{CartService} from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
-  product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+export class ProductDetailsComponent implements OnInit {
+product: Product | undefined;
+
+  addToCart(product:Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!')
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) {}
 
   ngOnInit() {
     //first get the product id from the current route
@@ -22,4 +33,5 @@ export class ProductDetailsComponent {
     //find the product that correspond with the id provided in route
     this.product = products.find((product) => product.id === productIdFromRoute);
   }
+
 }
